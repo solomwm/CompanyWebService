0e8c7936aed5058dd5f1048b172246e836bf17e8
solom.wm <solom.on.wm00017@gmail.com>
2018-12-17 23:23:38 +0300

    Init

---
+++ INNService/CompanyesDataEngine.cs
@@ -0,0 +1,116 @@
+﻿using INNService.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+
+namespace INNService
+{
+    public class CompanyesDataEngine
+    {
+        private List<Company> companyes;
+        private BinaryFormatter formatter;
+        private string _dataFilePath = String.Empty;
+        private int _currentId = 1;
+
+        public CompanyesDataEngine()
+        {
+            formatter = new BinaryFormatter();
+            companyes = new List<Company>();
+            _dataFilePath = Directory.GetCurrentDirectory() + @"\wwwroot\CompanyesDB.dat";
+        }
+
+        public CompanyesDataEngine(string dataFilePath): this()
+        {
+            _dataFilePath = dataFilePath;
+            using (FileStream stream = new FileStream(dataFilePath, FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    companyes = formatter.Deserialize(stream) as List<Company>;
+                    _currentId = getLastId() + 1;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        ~CompanyesDataEngine()
+        {
+            SaveData();
+        }
+
+        public List<Company> Companyes
+        {
+            get => companyes;
+            set => companyes = value;
+        }
+        public int CurrentId { get => _currentId; }
+
+        public Company GetCompany(int id)
+        {
+            return companyes.FirstOrDefault((c) => c.Id == id);
+        }
+
+        public Company GetByInn(string inn)
+        {
+            return companyes.FirstOrDefault((c) => c.INN.Equals(inn));
+        }
+
+        public IEnumerable<Company> GetByName(string name)
+        {
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+            return companyes.FindAll((c) => c.Name.IndexOf(name, comp) >= 0);
+        }
+
+        public void SaveData()
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(_dataFilePath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    formatter.Serialize(stream, companyes);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void EditOrCreate(Company company)
+        {
+            var cmp = companyes.FirstOrDefault((c) => c.Id == company.Id);
+            if (cmp != null)
+            {
+                cmp.INN = company.INN;
+                cmp.Name = company.Name;
+            }
+            else
+            {
+                company.Id = _currentId++;
+                companyes.Add(company);
+            }
+            SaveData();
+        }
+
+        public void Delete(int id)
+        {
+                var cmp = companyes.FirstOrDefault((c) => c.Id == id);
+                if (cmp != null)
+                {
+                    companyes.Remove(cmp);
+                    SaveData();
+                }      
+        }
+
+        private int getLastId()
+        {
+            return companyes.Max((c) => c.Id);
+        }
+    }
+}
---
+++ INNService/Controllers/CompanyController.cs
@@ -0,0 +1,103 @@
+﻿using INNService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INNService.Controllers
+{
+    [Route("api/[controller]")]
+    public class CompanyController : Controller
+    {
+        private Company[] baseCompanyes = {new Company {Id = 1, INN = "0000000000", Name = "OOO FuckeL" },
+            new Company {Id = 2, INN = "0000000001", Name = "OOO OneMore, OneMore..."} };
+        private CompanyesDataEngine dataEngine;
+
+        public CompanyController()
+        {
+            var dataFile = Directory.GetCurrentDirectory() + @"\wwwroot\CompanyesDB.dat";
+            if (System.IO.File.Exists(dataFile))
+            {
+                dataEngine = new CompanyesDataEngine(dataFile);
+            }
+            else
+            {
+                dataEngine = new CompanyesDataEngine()
+                {
+                    Companyes = baseCompanyes.ToList<Company>(),
+                };
+                dataEngine.SaveData();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Company> GetAll()
+        {
+            return dataEngine.Companyes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var company = dataEngine.GetCompany(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+        [HttpGet("getbyinn/{inn}")]
+        public IActionResult GetByInn(string inn)
+        {
+            var company = dataEngine.GetByInn(inn);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+        [HttpGet("getbyname/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var companyES = dataEngine.GetByName(name);
+            if (companyES == null || companyES.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(companyES);
+        }
+
+        [HttpGet("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            Company model;
+            if (id.HasValue)
+            {
+                model = dataEngine.GetCompany(id.Value);
+                if (model == null) return NotFound();
+            }
+            else model = new Company();
+            return Ok(model);
+        }
+
+        [HttpPut("edit/{id?}")]
+        public IActionResult Edit(Company model)
+        {
+            dataEngine.EditOrCreate(model);
+            return RedirectToAction(nameof(model.Id));
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            dataEngine.Delete(id);
+            return RedirectToAction(nameof(GetAll));
+        }
+    }
+}
