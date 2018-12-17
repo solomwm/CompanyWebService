3d55d8170047db8cdb28369bd3a37096e4f6b899
solom.wm <solom.on.wm00017@gmail.com>
2018-12-17 23:21:33 +0300

    Init

---
+++ ConsoleINNClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleINNClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleINNClient
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+        static string baseUrl = "http://localhost:55658/api/company";
+
+        static void Main(string[] args)
+        {
+            IEnumerable<Company> companys;
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            companys = await GetAllCompanyes(baseUrl);
+        }
+
+        static async Task<IEnumerable<Company>> GetAllCompanyes(string path)
+        {
+            IEnumerable<Company> comps;
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(baseUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var str = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
