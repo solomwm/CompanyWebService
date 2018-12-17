ded6a456dd06ebb2485f863b9588aa9aa97990f2
solom.wm <solom.on.wm00017@gmail.com>
2018-12-17 23:19:12 +0300

    Init

---
+++ CompanyWebService/CompanyesDataEngine.cs
@@ -0,0 +1,76 @@
+﻿using INNService.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+
+namespace INNService
+{
+    public class CompanyesDataEngine
+    {
+        private List<Company> companyes;
+        private BinaryFormatter formatter;
+        private string _dataFilePath;
+
+        public CompanyesDataEngine(string dataFilePath)
+        {
+            formatter = new BinaryFormatter();
+            _dataFilePath = dataFilePath;
+            using (FileStream stream = new FileStream(dataFilePath, FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    companyes = formatter.Deserialize(stream) as List<Company>;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        ~CompanyesDataEngine()
+        {
+            SaveData();
+        }
+
+        public List<Company> Companyes
+        {
+            get => companyes;
+            set => companyes = value;
+        }
+
+        public Company GetCompany(int id)
+        {
+            return companyes.FirstOrDefault((c) => c.Id == id);
+        }
+
+        public Company GetByInn(string inn)
+        {
+            return companyes.FirstOrDefault((c) => c.INN.Equals(inn));
+        }
+
+        public IEnumerable<Company> GetByName(string name)
+        {
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+            return companyes.FindAll((c) => c.Name.IndexOf(name, comp) >= 0);
+        }
+
+        public void SaveData()
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(_dataFilePath, FileMode.Open, FileAccess.Write))
+                {
+                    formatter.Serialize(stream, companyes);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ CompanyWebService/Controllers/CompanyController.cs
@@ -0,0 +1,91 @@
+﻿using INNService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INNService.Controllers
+{
+    [Route("company")]
+    public class CompanyController : Controller
+    {
+        private Company[] baseCompanyes = {new Company {Id = 1, INN = "0000000000", Name = "OOO FuckeL" },
+            new Company {Id = 2, INN = "0000000001", Name = "OOO OneMore, OneMore..."} };
+        private CompanyesDataEngine dataEngine;
+
+        public CompanyController()
+        {
+            var dataFile = Directory.GetCurrentDirectory() + @"\wwwroot\CompanyesDB.dat";
+            if (System.IO.File.Exists(dataFile))
+            {
+                dataEngine = new CompanyesDataEngine(dataFile);
+            }
+            else
+            {
+                dataEngine = new CompanyesDataEngine(dataFile)
+                {
+                    Companyes = baseCompanyes.ToList<Company>()
+                };
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return Redirect("GetAll");
+        }
+
+        public IEnumerable<Company> GetAll()
+        {
+            return dataEngine.Companyes;
+        }
+
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            var company = dataEngine.GetCompany(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+        [Route("getbyinn/{inn}")]
+        public IActionResult GetByInn(string inn)
+        {
+            var company = dataEngine.GetByInn(inn);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+        [Route("getbyname/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var companyES = dataEngine.GetByName(name);
+            if (companyES == null || companyES.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(companyES);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return Redirect(@"Get\" + id);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Company company)
+        {
+            return null;
+        }
+    }
+}
